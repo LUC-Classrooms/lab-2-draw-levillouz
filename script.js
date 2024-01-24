60f576d9383f351231905043365d6fdbff3d9c74
Lev Illouz <156964356+levillouz@users.noreply.github.com>
2024-01-24 16:47:38 +0000

    Draft 1

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 function setup(){
-  createCanvas(600, 400);
+  createCanvas(600, 800);
 }
 
 function draw(){
-  background(200);
-  // add your drawing code here ...
-  
+  background(0, 25, 50);
+  fill (255, 200, 0);
+ellipse (300, 400, 200, 200);
+strokeWeight(2);
+fill (255);
+  ellipse(325, 380, 20, 20);
+  ellipse(275, 380, 20, 20);
+  fill(0);
+  ellipse (325, 380, 5, 5);
+  ellipse(275, 380, 5, 5);
+  arc (300, 410, 100, 50, 0, 160, PI);
+  fill (0, 255, 0);
+  rect (0, 650, 800, 200);
+  fill (255);
+  rect (50, 570, 80, 80);
+  fill (0, 158, 255);
+  rect (58, 582, 20, 20);
+  fill (0, 158, 255);
+  rect (100, 582, 20, 20);
+  fill(0);
+  rect (79, 615, 20, 35);
+  fill (0);
+  rect (61, 530, 20, 30);
+  fill (255, 0, 0);
+  triangle (90, 530, 130, 570, 50, 570);
+  fill (255);
+  ellipse (65, 495, 10, 70)
 }
